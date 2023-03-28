a7cfb5d54ec6a01262ceb4f3fd5d6065fe5fe98d
Tetiankaa <tanyakorsun0208@gmail.com>
2023-03-28 21:02:19 +0200

    Initial commit

---
+++ src/main/java/ua/com/owu/springboot_lessons/SpringbootLessonsApplication.java
@@ -0,0 +1,13 @@
+package ua.com.owu.springboot_lessons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLessonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootLessonsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ua/com/owu/springboot_lessons/SpringbootLessonsApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.com.owu.springboot_lessons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootLessonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
